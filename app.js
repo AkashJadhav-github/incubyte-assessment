1e785165880d4652086fb3d428da5b11223a4529
AkashJadhav-github <31062143+AkashJadhav-github@users.noreply.github.com>
2024-12-31 11:37:05 +0530

    Handled the condition for variable delimiter in the string.

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ function add(numbers) {
   
   // Convert the string numbers to integers and calculate the sum
   let sum = num_array.reduce((acc, x) => acc + parseInt(x), 0);
-  console.log(sum)
+
   // Return the sum
   return sum; 
 }
 
-add("2\n,2,3");
+add("//.2\n.2.3");
 
 function getNumbersFromString(numbers) {
+	// Check the delimiter in string
+  let delimiter = numbers.startsWith("//") ? numbers[2] : ','
+  
 	// Clean the string
-  let cleanedStr = numbers.replace(/\n/g, '');
+  let cleanedStr = numbers.replace(/\/\/./g, '').replace(/\n/g, '');
 
   // Split the string based on commas to extract individual number strings
-  let num_array = cleanedStr.split(",");
+  let num_array = cleanedStr.split(delimiter);
   
   return num_array;
 }
