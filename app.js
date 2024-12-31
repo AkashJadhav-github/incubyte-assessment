dba55220d66cb57fcf54c473a9058f8a94ce9bb1
AkashJadhav-github <31062143+AkashJadhav-github@users.noreply.github.com>
2024-12-31 11:10:20 +0530

    Added stage for cleaning of the string. removes \n from the string

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ function add(numbers) {
   
   // Convert the string numbers to integers and calculate the sum
   let sum = num_array.reduce((acc, x) => acc + parseInt(x), 0);
-  
+  console.log(sum)
   // Return the sum
   return sum; 
 }
 
-add("1,2,3");
+add("2\n,2,3");
 
 function getNumbersFromString(numbers) {
+	// Clean the string
+  let cleanedStr = numbers.replace(/\n/g, '');
+
   // Split the string based on commas to extract individual number strings
-  let num_array = numbers.split(",");
+  let num_array = cleanedStr.split(",");
+  
   return num_array;
 }
